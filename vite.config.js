fee748b94e0859ea5231b4a654b2cf58ad11b62f
Vinit Kumar Pothuraju <vinitkumarpothuraju@gmail.com>
2025-07-20 11:58:41 +0530

    Update Json file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Nike-Web/', 
   plugins: [react()],
-  base: '/Nike-Web/', // Ensure this matches your repo name
 });
